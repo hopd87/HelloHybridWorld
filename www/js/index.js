7b072272dbd73cf53871c9a2899b1b5c0db396ca
Dinesh <dineshkumar.pandian@emc.com>
2016-03-17 15:48:57 -0400

    Uncommented app.initialize

--- www/js/index.js
+++ www/js/index.js
@@ -135,7 +135,7 @@ var app = {
     getContacts: function() {
     	
     	// Get ENTERPRISE Contacts
-    	$.getJSON("http://192.168.1.23:8080/ETAPP-REST-1/contacts", function(contacts) {
+    	$.getJSON("http://192.168.1.23:8080/ETAPPREST-1/contacts", function(contacts) {
     	    $("#enterprise_contacts_listview").empty();
     	    var items = [];
     	    var contactItem = "";
@@ -242,4 +242,4 @@ var app = {
  * BOOTSTRAP
  * 
  *******************************************************************/
-//app.initialize();
+app.initialize();
